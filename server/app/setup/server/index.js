99bc84ffda50ae2361c38cc9eed78e4b4dd59a6a
Matheus Almeida <almeida-matheus@hotmail.com.br>
2019-02-20 15:30:30 -0300

    adjustments

--- server/app/setup/server/index.js
+++ server/app/setup/server/index.js
@@ -44,6 +44,12 @@ module.exports = (app) => {
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
+	app.use('/', (req, res) => {
+		res.status(200)
+			.json({
+				success: true
+			});
+	});
 
 	expressRoutes(app);
 
