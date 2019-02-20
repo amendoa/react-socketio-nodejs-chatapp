909cb66653f78bb2442ddd126e276706a5f8d755
Matheus Almeida <almeida-matheus@hotmail.com.br>
2019-02-20 17:53:38 -0300

    deploy

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 const authRoute = absoluteRequire('routes/auth');
 const contactRoute = absoluteRequire('routes/contact');
 const messageRoute = absoluteRequire('routes/message');
+const defaultRoute = absoluteRequire('routes/default');
 const conversationRoute = absoluteRequire('routes/conversation');
 const winstonMiddleware = absoluteRequire('middlewares/winston');
 const jwtMiddleware = absoluteRequire('middlewares/jwt');
@@ -12,4 +13,5 @@ module.exports = (app) => {
 	app.use(contactRoute);
 	app.use(messageRoute);
 	app.use(conversationRoute);
+	app.use(defaultRoute);
 };
