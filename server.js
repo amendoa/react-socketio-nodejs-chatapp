9c95aecd51095daf739757e3cd6bc2b84b28bc51
Matheus Almeida <almeida-matheus@hotmail.com.br>
2019-02-20 14:19:22 -0300

    deploy test

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
+const http = require('http');
 
 var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
-app.use('/', (req, res) => {
-  res.status(200).json({success: true});
+
+const server = http.createServer(app);
+
+server.listen(server_port, server_ip_address, () => {
+  
 });
 
-app.listen(server_port, server_ip_address);
+app.use('/', (req, res) => {
+  res.status(200).json({success: true, message: 'testeeeeeeeeeeee'});
+});
+
 
 module.exports = app;
