85556af8f56d4a8bef15179ea8cd1e37337fef5f
Matheus Almeida <almeida-matheus@hotmail.com.br>
2019-02-20 15:54:04 -0300

    new deploy

--- app/setup/mongoose/index.js
+++ app/setup/mongoose/index.js
@@ -4,12 +4,10 @@ const constants = absoluteRequire('modules/constants');
 const logger = absoluteRequire('modules/winston');
 
 module.exports = () => {
-	const URI = (
-		process.env.OPENSHIFT_MONGODB_DB_URL
-			|| process.env.MONGO_URL
-			|| constants.MONGOOSE.URI
-	)
-	+ constants.MONGOOSE.DB;
+	const MONGO_HOST = process.env.OPENSHIFT_MONGODB_DB_HOST || constants.MONGOOSE.HOST;
+	const MONGO_PORT = process.env.OPENSHIFT_MONGODB_DB_PORT || constants.MONGOOSE.PORT;
+
+	const URI = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${constants.MONGOOSE.DB}`;
 
 	const MONGOOSE_OPTIONS = {
 		useNewUrlParser: true,
